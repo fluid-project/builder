75d3b334b9038065fe06c2c7412109788b6e5e04
Justin Obara <obarajustin@gmail.com>
2010-01-06 17:11:03 +0000

    FLUID-3458: committing Laurel's patch (FLUID-3458-1.patch.txt) which changes the buildscripts to make the cleanup script executable and also adjusts the cleanup script to cleanup the proper directory (due to a name change in a previous commit).
    svn path=/fluid/infrastructure/infusionBuilder/trunk/; revision=8957

--- infusionBuilder-secure/scripts/cleanup.sh
+++ infusionBuilder-secure/scripts/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find ~/website/infusion-builder-secure/tmp/build/ -mtime +1 -exec rm -Rf {} \;
-find ~/website/infusion-builder-secure/tmp/products/ -mtime +1 -exec rm -Rf {} \;
+find ~/website/infusionBuilder-secure/tmp/build/ -mtime +1 -exec rm -Rf {} \;
+find ~/website/infusionBuilder-secure/tmp/products/ -mtime +1 -exec rm -Rf {} \;
